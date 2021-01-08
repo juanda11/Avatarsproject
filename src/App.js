0010cf6be86a707b4ba79d6ceb5cef0e665d1f8c
juanda11 <38761346+juanda11@users.noreply.github.com>
2021-01-07 19:19:53 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Aquí inicia mi proyecto, Avatars </h1>
+      <h2>Here starts my project, Avatars</h2>
+    </div>
+  );
+}
